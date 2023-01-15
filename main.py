a7f6711e3da1e2baf39f8d305e21f70d30738c54
Farouk <faiz@eurecom.fr>
2023-01-15 21:30:20 +0100

    Print unscored (if any) and top 10 posts

--- main.py
+++ main.py
@@ -22,9 +22,18 @@ def parse_post(rows: Any, index: int) -> Post: # TODO: Better typing for rows ?
     second_row = rows[index * 3 + 1]
 
     score_span = second_row.find('span', class_='score')
-    score = int(score_span.text.split(" ")[0]) if score_span else -1 # TODO: Handle posts without score (i.e. -1)
+    score = int(score_span.text.split(" ")[0]) if score_span else -1
 
     date = datetime.fromisoformat(second_row.find('span', class_='age')['title'])
     return Post(title, url, score, date)
     
 posts = [parse_post(rows, i) for i in range(NUM_NEWS_PER_PAGE)]
+posts_without_score = list(filter(lambda p: p.score == -1, posts))
+posts.sort(key=lambda p: p.score, reverse= True)
+
+if len(posts_without_score):
+    print("Posts without score:")
+    print(*posts_without_score, sep="\n")
+    print("\nPosts with score:")
+
+print(*posts[:10], sep="\n")
