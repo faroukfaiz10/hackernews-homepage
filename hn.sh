83bec6846d8df368b14cb185916a9e5332ddff49
Farouk <faiz@eurecom.fr>
2023-11-18 21:14:32 +0100

    Persist result & rename venv dir

--- hn.sh
+++ hn.sh
@@ -2,5 +2,5 @@
 
 BASEDIR=$(dirname "$0")
 
-source $BASEDIR/venv/bin/activate
-python $BASEDIR/src/main.py
+source $BASEDIR/.venv/bin/activate
+python $BASEDIR/src/main.py | tee ~/.hn/$(date +%F_%T).txt
